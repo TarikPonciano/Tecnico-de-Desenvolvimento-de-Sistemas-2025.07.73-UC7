1223dc8c6ce56b56b2e1b912852868cd419affdf
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-09-24 11:01:47 -0300

    Código Tabela

--- Aula 23-09/Uso do LocalStorage/scripts.js
+++ Aula 23-09/Uso do LocalStorage/scripts.js
@@ -10,7 +10,7 @@ function cadastrarPessoa(e) {
 
     const pessoa = { "nome": nome, "idade": idade, "altura": altura }
 
-    const pessoas = JSON.parse(localStorage.getItem("pessoas"))
+    const pessoas = JSON.parse(localStorage.getItem("pessoas")) || []
 
     pessoas.push(pessoa)
 
@@ -22,17 +22,47 @@ function cadastrarPessoa(e) {
 
 // Adaptar a função para carregar a lista de pessoas do LocalStorage e exibir TODAS no navegador atráves do HTML.
 function carregarDados() {
-    const infoNome = document.getElementById("info-nome")
-    const infoIdade = document.getElementById("info-idade")
-    const infoAltura = document.getElementById("info-altura")
+    // const infoNome = document.getElementById("info-nome")
+    // const infoIdade = document.getElementById("info-idade")
+    // const infoAltura = document.getElementById("info-altura")
 
-    infoNome.innerText = `Nome: ${localStorage.getItem("nome")}`
-    infoIdade.innerText = `Idade: ${localStorage.getItem("idade")}`
-    infoAltura.innerText = `Altura: ${localStorage.getItem("altura")}`
+    // infoNome.innerText = `Nome: ${localStorage.getItem("nome")}`
+    // infoIdade.innerText = `Idade: ${localStorage.getItem("idade")}`
+    // infoAltura.innerText = `Altura: ${localStorage.getItem("altura")}`
+    const pessoas = JSON.parse(localStorage.getItem("pessoas")) || []
+    const corpoTabelaHtml = document.querySelector("#tabela-pessoas tbody")
+
+    pessoas.forEach(pessoa => {
+        const botao = document.createElement("button")
+        botao.addEventListener("click", () => {
+            if (pessoa.idade >= 18) {
+                alert("Essa pessoa é maior de idade!")
+            } else {
+                alert("Essa pessoa é menor de idade!")
+            }
+        })
+        botao.innerText = "Verificar Idade"
+
+        const linha = document.createElement("tr")
+        linha.innerHTML = `
+        <td>${pessoa.nome}</td>
+        <td>${pessoa.idade}</td>
+        <td>${pessoa.altura}</td>
+        `
+
+        linha.appendChild(document.createElement("td").appendChild(botao))
+
+        corpoTabelaHtml.appendChild(linha)
+
+    });
+
+
 }
 
 carregarDados()
 
+
+// Propor um cenário de cadastro de alguma coisa(Livro, Pessoa, Funcionário). O formulário de cadastro deve possuir pelo menos 5 campos distintos. Crie um sistema que cadastrar esses itens no LocalStorage e depois exiba esses itens em uma tabela contendo todas as informações de cada item. Além disso, crie um botão para cada item que permite visualizar suas informações em um alert. Crie um botão de remover para cada item que remove aquele item do LocalStorage. Implementar alguma categoria de filtragem (Ex: cargo de funcionário, departamento).
 
 // const lista = ["Banana", "Maçã", "Pêra"]
 
