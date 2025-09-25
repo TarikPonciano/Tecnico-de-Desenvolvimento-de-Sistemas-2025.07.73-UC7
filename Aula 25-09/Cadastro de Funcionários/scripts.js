8d0cb62c5f5174d2a21c4a3a1dc5ef3ee83f5304
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-09-25 10:29:56 -0300

    Formulário Exibindo e Estilizado

--- Aula 25-09/Cadastro de Funcionários/scripts.js
+++ Aula 25-09/Cadastro de Funcionários/scripts.js
@@ -19,3 +19,29 @@ function cadastrarFuncionario(e) {
     localStorage.setItem("funcionarios", JSON.stringify(funcionarios))
 }
 
+//Criar a função renderizarTabela:
+// 1 - Capturar informação do localStorage(getItem)
+// 2 - Utilizar forEach para percorrer cada elemento da lista do localStorage
+// 3 - Dentro do forEach criar um elemento <tr></tr> e dentro desse elemento criar os elementos <td></td> para cada informação do item que deseja exibir
+// 4 - Adicionar os elementos criados ao tbody da tabela de exibição
+
+function renderizarTabela() {
+
+    const funcionarios = JSON.parse(localStorage.getItem("funcionarios")) || []
+    const corpoTabelaHtml = document.querySelector("#tabela-funcionarios tbody")
+
+    funcionarios.forEach(funcionario => {
+        const linha = document.createElement("tr")
+        linha.innerHTML = `
+        <td>${funcionario.id}</td>
+        <td>${funcionario.nome}</td>
+        <td>${Number(funcionario.salario).toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}</td>
+        <td>${funcionario.cargo}</td>
+        <td>${funcionario.departamento}</td>
+        `
+        corpoTabelaHtml.appendChild(linha)
+    });
+
+}
+
+renderizarTabela()
