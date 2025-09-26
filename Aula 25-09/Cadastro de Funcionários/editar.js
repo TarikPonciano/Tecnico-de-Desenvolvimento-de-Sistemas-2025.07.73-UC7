02f2d1a8de02029bbe60a4d0ce22fd13588f4533
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-09-26 10:24:58 -0300

    Editar implementado

--- Aula 25-09/Cadastro de Funcionários/editar.js
+++ Aula 25-09/Cadastro de Funcionários/editar.js
@@ -1,8 +1,34 @@
+function atualizarFuncionario(e) {
+    
+    const id = document.getElementById("campo-id").value
+    const nome = document.getElementById("campo-nome").value
+    const salario = document.getElementById("campo-salario").value
+    const cargo = document.getElementById("campo-cargo").value
+    const departamento = document.getElementById("campo-departamento").value
+
+    const funcionarios = JSON.parse(localStorage.getItem("funcionarios"))
+
+    const funcionarioEscolhido = funcionarios.find((f) => f.id == id)
+
+    funcionarioEscolhido.nome = nome
+    funcionarioEscolhido.salario = salario
+    funcionarioEscolhido.cargo = cargo
+    funcionarioEscolhido.departamento = departamento
+
+    localStorage.setItem("funcionarios", JSON.stringify(funcionarios))
+
+    window.location.href = "index.html"
+
+}
+
+
 function carregarPagina() {
 
     const campoNome = document.getElementById("campo-nome")
     const campoId = document.getElementById("campo-id")
+    const campoSalario = document.getElementById("campo-salario")
     const campoCargo = document.getElementById("campo-cargo")
+    const campoDepartamento = document.getElementById("campo-departamento")
 
     const params = new URLSearchParams(window.location.search)
     const info = params.get("id")
@@ -13,7 +39,9 @@ function carregarPagina() {
 
     campoId.value = funcionarioEscolhido.id
     campoNome.value = funcionarioEscolhido.nome
+    campoSalario.value = funcionarioEscolhido.salario
     campoCargo.value = funcionarioEscolhido.cargo
+    campoDepartamento.value = funcionarioEscolhido.departamento
 
 }
 
